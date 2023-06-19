8a792a59699d84ec120e9a90432b133d550de686
Eduardo Expósito Barrera <eexposito@opencanarias.com>
2023-06-19 15:10:17 +0100

    style(TAP-1414): New format for the community

---
+++ sidebarsCommunity.js
@@ -0,0 +1,44 @@
+module.exports = {
+    community: [
+        {
+            type: 'doc',
+            label: 'Disclaimer',
+            id: 'DISCLAIMER',
+        },
+        {
+            type: 'doc',
+            label: 'Repositories',
+            id: 'repositories',
+        },
+        {
+            type: 'doc',
+            label: 'Licenses',
+            id: 'licenses',
+        },
+        {
+            type: 'doc',
+            label: 'Support',
+            id: 'SUPPORT',
+        },
+        {
+            type: 'doc',
+            label: 'Contributing',
+            id: 'CONTRIBUTING',
+        },
+        {
+            type: 'doc',
+            label: 'Security',
+            id: 'SECURITY',
+        },
+        {
+            type: 'doc',
+            label: 'Code of conduct',
+            id: 'CODE_OF_CONDUCT',
+        },
+        {
+            type: 'doc',
+            label: 'European Funding',
+            id: 'european-funding',
+        },
+    ],
+};
