2e70534021bcb915ae645bc5e1a5162e3e57369e
TAPLECICD <taple@opencanarias.com>
2023-07-20 11:34:16 +0000

    doc: Crate documentation updated

--- static/crate-doc/next/taple_core/crypto/sidebar-items.js
+++ static/crate-doc/next/taple_core/crypto/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["KeyPair","Payload"],"fn":["check_cryptography","create_seed","generate"],"struct":["BaseKeyPair","CryptoBox"],"trait":["DHKE","DSA","KeyGenerator","KeyMaterial"],"type":["Ed25519KeyPair","Secp256k1KeyPair"]};
+window.SIDEBAR_ITEMS = {"enum":["KeyPair","Payload"],"fn":["create_seed","generate"],"struct":["BaseKeyPair","CryptoBox"],"trait":["DHKE","DSA","KeyGenerator","KeyMaterial"],"type":["Ed25519KeyPair","Secp256k1KeyPair"]};
